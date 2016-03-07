ab1c69c1cb500cd92a7800a321eef9f856895c6f
Rune Flobakk <flobakk@gmail.com>
2016-03-07 14:38:14 +0100

    Convenience factorymethod for a single exit-url + doc

--- src/main/java/no/digipost/signature/client/direct/ExitUrls.java
+++ src/main/java/no/digipost/signature/client/direct/ExitUrls.java
@@ -17,6 +17,30 @@ package no.digipost.signature.client.direct;
 
 public final class ExitUrls implements WithExitUrls {
 
+    /**
+     * A single exit url can be used if you do not need to separate
+     * resources for handling the different outcomes of a direct job.
+     * This is simply a convenience factory method for
+     * {@link ExitUrls#of(String, String, String)} with the same url
+     * given for all the arguments.
+     *
+     * @param url The url you want the user to be redirected to upon
+     *        completing the signing ceremony, regardless of its outcome
+     */
+    public static ExitUrls singleExitUrl(String url) {
+        return of(url, url, url);
+    }
+
+    /**
+     * Specify the urls the user is will be redirected to for different
+     * outcomes of a signing ceremony. When the user is redirected, the urls will
+     * have an appended query parameter ({@link StatusReference#STATUS_QUERY_TOKEN_PARAM_NAME})
+     * which contains a token required to {@link DirectClient#getStatus(StatusReference) query for the status of the job}.
+     *
+     * @param completionUrl the user will be redirected to this url after having successfully signed the document.
+     * @param rejectionUrl the user will be redirected to this url if actively rejecting to sign the document.
+     * @param errorUrl the user will be redirected to this url if any unexpected error happens during the signing ceremony.
+     */
     public static ExitUrls of(String completionUrl, String rejectionUrl, String errorUrl) {
         return new ExitUrls(completionUrl, rejectionUrl, errorUrl);
     }
