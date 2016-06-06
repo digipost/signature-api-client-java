2b5048da6780e176b5ed8f8b6e06c992a5fe9482
Sindre Nordbø <sindre.nordbo@bekk.no>
2016-06-06 09:05:22 +0200

    Revert "Endret DirectJobStatus fra enum til en samling konstanter"
    This reverts commit 71a4a7967d63446abc594d97ae4a78d701afe49a.
    Revertert endringen også i API-spesifikasjonen. Det var en unødvendig endring som førte til brekt bakoverkompatibilitet.

--- src/main/java/no/digipost/signature/client/direct/DirectJobStatus.java
+++ src/main/java/no/digipost/signature/client/direct/DirectJobStatus.java
@@ -15,86 +15,50 @@
  */
 package no.digipost.signature.client.direct;
 
-import java.util.List;
-import java.util.Objects;
+import no.digipost.signature.api.xml.XMLDirectSignatureJobStatus;
 
-import static java.util.Arrays.asList;
-
-public final class DirectJobStatus {
+public enum DirectJobStatus {
 
     /**
-     * The user decided to reject to sign the document, and has been redirected to the
-     * rejection-url provided in the direct-signature-job-request's exit-urls.
+     * The document(s) of the job has been signed by the receiver.
+     *
+     * @see XMLDirectSignatureJobStatus#SIGNED
      */
-    public static final DirectJobStatus REJECTED = new DirectJobStatus("REJECTED");
+    SIGNED,
 
+    /**
+     * The signature job has been rejected by the receiver.
+     *
+     * @see XMLDirectSignatureJobStatus#REJECTED
+     */
+    REJECTED,
+
+    /**
+     * An error occured during the signing ceremony.
+     *
+     * @see XMLDirectSignatureJobStatus#FAILED
+     */
+    FAILED,
     /**
      * The user didn't sign the document before the job expired.
+     *
+     * @see XMLDirectSignatureJobStatus#EXPIRED
      */
-    public static final DirectJobStatus EXPIRED = new DirectJobStatus("EXPIRED");
+    EXPIRED,
 
     /**
-     * The document has been signed, and the signer has been redirected to the
-     * completion-url provided in the direct-signature-job-request's exit-urls.
-     * The signed document artifacts can be downloaded by following the appropriate
-     * urls in the direct-signature-job-status-response.
-     */
-    public static final DirectJobStatus SIGNED = new DirectJobStatus("SIGNED");
-
-    /**
-     * An unexpected error occured during the signing ceremony, and the user has been redirected to the
-     * error-url provided in the direct-signature-job-request's exit-urls.
-     */
-    public static final DirectJobStatus FAILED = new DirectJobStatus("FAILED");
-
-	/**
      * There has not been any changes since the last received status change.
      */
-    public static final DirectJobStatus NO_CHANGES = new DirectJobStatus("NO_CHANGES");
+    NO_CHANGES;
 
-    private static final List<DirectJobStatus> KNOWN_STATUSES = asList(
-            REJECTED,
-            EXPIRED,
-            SIGNED,
-            FAILED
-    );
-
-    private final String identifier;
-
-    public DirectJobStatus(String identifier) {
-        this.identifier = identifier;
-    }
-
-    public static DirectJobStatus fromXmlType(String xmlDirectJobStatus) {
-        for (DirectJobStatus status : KNOWN_STATUSES) {
-            if (status.is(xmlDirectJobStatus)) {
-                return status;
-            }
+    public static DirectJobStatus fromXmlType(XMLDirectSignatureJobStatus xmlJobStatus) {
+        switch (xmlJobStatus) {
+            case SIGNED:   return SIGNED;
+            case REJECTED: return REJECTED;
+            case FAILED:   return FAILED;
+            case EXPIRED:  return EXPIRED;
+            default:       throw new IllegalArgumentException("Unexpected status: " + xmlJobStatus);
         }
-
-        return new DirectJobStatus(xmlDirectJobStatus);
     }
 
-    private boolean is(String xmlDirectJobStatus) {
-        return this.identifier.equals(xmlDirectJobStatus);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof DirectJobStatus) {
-            DirectJobStatus that = (DirectJobStatus) o;
-            return Objects.equals(identifier, that.identifier);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(identifier);
-    }
-
-    @Override
-    public String toString() {
-        return identifier;
-    }
 }
--- src/test/java/no/digipost/signature/client/direct/DirectJobStatusTest.java
+++ src/test/java/no/digipost/signature/client/direct/DirectJobStatusTest.java
@@ -15,15 +15,23 @@
  */
 package no.digipost.signature.client.direct;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
-import no.digipost.signature.client.portal.SignatureStatus;
+import no.digipost.signature.api.xml.XMLDirectSignatureJobStatus;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
 public class DirectJobStatusTest {
 
     @Test
-    public void equals_and_hashCode() {
-        EqualsVerifier.forClass(DirectJobStatus.class).verify();
+    public void ableToConvertAllStatusesFromXsd() {
+        List<DirectJobStatus> convertedStatuses = new ArrayList<>();
+        for (XMLDirectSignatureJobStatus xmlStatus : XMLDirectSignatureJobStatus.values()) {
+            convertedStatuses.add(DirectJobStatus.fromXmlType(xmlStatus));
+        }
+        assertThat(convertedStatuses, hasSize(XMLDirectSignatureJobStatus.values().length));
     }
-
 }
