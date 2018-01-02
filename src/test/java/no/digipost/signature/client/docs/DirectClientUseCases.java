305e67e216229ae7b71e03b833f3f993288dddfd
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-02 14:58:45 +0100

    Legg til header, ignores og gir filer nytt navn - igjen

--- src/test/java/no/digipost/signature/client/docs/DirectClientUseCases.java
+++ src/test/java/no/digipost/signature/client/docs/DirectClientUseCases.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.digipost.signature.client.docs;
 
 import no.digipost.signature.client.ClientConfiguration;
@@ -21,7 +36,7 @@ import no.digipost.signature.client.security.KeyStoreConfig;
 import java.io.InputStream;
 
 @SuppressWarnings({"unused", "ConstantConditions", "StatementWithEmptyBody"})
-class DirectClientExamples {
+class DirectClientUseCases {
 
     static void create_client_configuration() {
         InputStream keyStore = null; // Stream created from keyStore file
