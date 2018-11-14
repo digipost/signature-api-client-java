be36e3f3ac6afd32915a514c2999a4482edb4374
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-11-14 09:31:31 +0100

    Legger til en enkel førsteverson av smoketester
    Masse manuelle greier for lasting av sertifikater, men den skal på sikt
    kunne lastes automagisk med sertifikater i repo.

--- src/test/java/no/digipost/signature/client/SmokeTests.java
+++ src/test/java/no/digipost/signature/client/SmokeTests.java
@@ -1,6 +1,7 @@
 package no.digipost.signature.client;
 
 import no.digipost.signature.client.core.Sender;
+import no.digipost.signature.client.core.exceptions.KeyException;
 import no.digipost.signature.client.direct.DirectClient;
 import no.digipost.signature.client.direct.DirectDocument;
 import no.digipost.signature.client.direct.DirectJob;
@@ -31,18 +32,45 @@ public class SmokeTests {
         String keyStorePassword = System.getenv("ORG_KEY_STORE_PASSWORD");
         String privateKeyPassword = System.getenv("ORG_PRIVATE_KEY_PASSWORD");
 
-        return KeyStoreConfig.fromKeyStore(
-                getClass().getResourceAsStream("/client-keystore.jce"),
-                "posten norge as", keyStorePassword, privateKeyPassword
+        KeyStoreConfig keyStore;
+        try {
+            keyStore = KeyStoreConfig.fromJavaKeyStore(
+                    getClass().getResourceAsStream("/client-keystore.jce"),
+                    "posten norge as", keyStorePassword, privateKeyPassword
+            );
+
+        } catch (KeyException e) {
+            throw new RuntimeException(
+                    "To test the integration directly with Signering backend, please add the 'client-keystore.jce' from backend, " +
+                            "and set key store and private key password as environment variables to avoid committing them:" +
+                            "export ORG_KEY_STORE_PASSWORD={the keystore password, is in backend code}" +
+                            "export ORG_PRIVATE_KEY_PASSWORD={the private key password, is in backend code}");
+        }
+
+        return keyStore;
+    }
+
+    private KeyStoreConfig pkcs12Keystore() {
+        String privateKeyPassword = System.getenv("ORG_PRIVATE_KEY_PKCS12_BRING");
+        KeyStoreConfig keyStoreConfig = KeyStoreConfig.fromOrganizationCertificate(getClass().getResourceAsStream("/Brig_Digital_Signature_Key_Encipherment_Data_Encipherment.p12"), privateKeyPassword);
+
+        return keyStoreConfig;
+    }
+
+    private KeyStoreConfig originalKeystoreWithPkcs12() {
+        String privateKeyPassword = System.getenv("ORG_PRIVATE_KEY_PKCS12_BRING");
+
+        return KeyStoreConfig.fromJavaKeyStore(
+                getClass().getResourceAsStream("/Bring_Digital_Signature_Key_Encipherment_Data_Encipherment.p12"),
+                "digipost testintegrasjon for digital post", privateKeyPassword, privateKeyPassword
         );
-
-
     }
 
     @Test
     public void do_external_call() throws IOException {
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder(keystore())
-                .globalSender(new Sender("984661185"))
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder(pkcs12Keystore())
+                .globalSender(new Sender("988015814"))      //Bring
+//                .globalSender(new Sender("984661185"))                    //Digipost
                 .trustStore(Certificates.TEST)
                 .serviceUri(ServiceUri.DIFI_TEST)
                 .serviceUri(URI.create(BASE_API_URL + "/api"))
@@ -66,10 +94,6 @@ public class SmokeTests {
         DirectJobResponse signatureJobResponse = directClient.create(signatureJob);
 
         System.out.println(signatureJobResponse.getSingleRedirectUrl());
-
-//        SignatureJobStatusResponse status = directClient.getStatus(new SignatureJobReference(signatureJobResponse.getStatusUrl()));
-//        InputStream pAdES = directClient.getPAdES(status.getpAdESUrl());
-//        IOUtils.copy(pAdES, new FileOutputStream("/tmp/pades.pdf"));
 
     }
 }
