b7f4e71c911821ff6ec89f0442b08ba3ed8bdec5
Rune Flobakk <flobakk@gmail.com>
2021-10-01 00:43:01 +0200

    Trust Buypass SEID2 enterprise certificates
    Modifies the alias-generating for certificate to include the name of the containing
    folder, because the file names for test and production CA certs are the
    same, and to be able to reliably test that the trusts are containing the
    expected certifiates. The aliases are not used by the client.

--- src/main/java/no/digipost/signature/client/Certificates.java
+++ src/main/java/no/digipost/signature/client/Certificates.java
@@ -10,13 +10,27 @@ public enum Certificates {
     TEST(
             "test/Buypass_Class_3_Test4_CA_3.cer",
             "test/Buypass_Class_3_Test4_Root_CA.cer",
+
+            "test/BPCl3CaG2HTBS.cer",
+            "test/BPCl3CaG2STBS.cer",
+            "test/BPCl3RootCaG2HT.cer",
+            "test/BPCl3RootCaG2ST.cer",
+
             "test/commfides_test_ca.cer",
             "test/commfides_test_root_ca.cer",
+
             "test/digipost_test_root_ca.cert.pem"
+
     ),
     PRODUCTION(
             "prod/BPClass3CA3.cer",
             "prod/BPClass3RootCA.cer",
+
+            "prod/BPCl3CaG2HTBS.cer",
+            "prod/BPCl3CaG2STBS.cer",
+            "prod/BPCl3RootCaG2HT.cer",
+            "prod/BPCl3RootCaG2ST.cer",
+
             "prod/commfides_ca.cer",
             "prod/commfides_root_ca.cer"
     );
--- src/main/java/no/digipost/signature/client/core/internal/security/TrustStoreLoader.java
+++ src/main/java/no/digipost/signature/client/core/internal/security/TrustStoreLoader.java
@@ -1,16 +1,17 @@
 package no.digipost.signature.client.core.internal.security;
 
-import no.digipost.signature.client.Certificates;
 import no.digipost.signature.client.core.exceptions.ConfigurationException;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -18,6 +19,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.isDirectory;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.StreamSupport.stream;
 
 public class TrustStoreLoader {
 
@@ -31,93 +38,103 @@ public class TrustStoreLoader {
             }
 
             return trustStore;
-        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException | KeyManagementException e) {
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
             throw new ConfigurationException("Unable to load certificates into truststore", e);
         }
     }
 
-    private static void loadCertificatesInto(String certificateFolder, final KeyStore trustStore) throws IOException, CertificateException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    private static void loadCertificatesInto(String certificateLocation, KeyStore trustStore) {
         ResourceLoader certificateLoader;
-        if (certificateFolder.indexOf("classpath:") == 0) {
-            certificateLoader = new ClassPathFileLoader(certificateFolder);
+        if (certificateLocation.startsWith(ClassPathResourceLoader.CLASSPATH_PATH_PREFIX)) {
+            certificateLoader = new ClassPathResourceLoader(certificateLocation);
         } else {
-            certificateLoader = new FileLoader(certificateFolder);
+            certificateLoader = new FileLoader(certificateLocation);
         }
 
-        certificateLoader.forEachFile(new ForFile() {
-            @Override
-            void call(String fileName, InputStream contents) {
-                try {
-                    X509Certificate ca = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(contents);
-                    trustStore.setCertificateEntry(fileName, ca);
-                } catch (CertificateException | KeyStoreException e) {
-                    throw new ConfigurationException("Unable to load certificate in " + fileName);
-                }
-            }
+        certificateLoader.forEachFile((fileName, contents) -> {
+            X509Certificate ca = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(contents);
+            trustStore.setCertificateEntry(fileName, ca);
         });
 
+        try {
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(trustStore);
 
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        tmf.init(trustStore);
-
-        SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, tmf.getTrustManagers(), null);
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, tmf.getTrustManagers(), null);
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            throw new ConfigurationException("Error initializing SSLContext for certification location " + certificateLocation, e);
+        }
     }
 
-    private static class ClassPathFileLoader implements ResourceLoader {
+
+    private static class ClassPathResourceLoader implements ResourceLoader {
 
         static final String CLASSPATH_PATH_PREFIX = "classpath:";
 
-        private final String certificatePath;
+        private final String resourceName;
 
-        ClassPathFileLoader(String certificateFolder) {
-            this.certificatePath = certificateFolder.substring(CLASSPATH_PATH_PREFIX.length());
+        ClassPathResourceLoader(String resourceName) {
+            this.resourceName = resourceName.replaceFirst(CLASSPATH_PATH_PREFIX, "");
         }
 
         @Override
-        public void forEachFile(ForFile forEachFile) throws IOException {
-            URL contentsUrl = Certificates.class.getResource(certificatePath);
-            if (contentsUrl == null) {
-                throw new ConfigurationException(certificatePath + " was not found");
+        public void forEachFile(ForFile forEachFile) {
+            URL resourceUrl = TrustStoreLoader.class.getResource(resourceName);
+            if (resourceUrl == null) {
+                throw new ConfigurationException(resourceName + " not found on classpath");
             }
-            try (InputStream inputStream = contentsUrl.openStream()){
-                forEachFile.call(new File(contentsUrl.getFile()).getName(), inputStream);
+
+            try (InputStream inputStream = resourceUrl.openStream()) {
+                forEachFile.call(generateAlias(Paths.get(resourceUrl.getPath())), inputStream);
+            } catch (Exception e) {
+                throw new ConfigurationException("Unable to load certificate from classpath: " + resourceName, e);
             }
         }
     }
 
     private static class FileLoader implements ResourceLoader {
-        private final File path;
+        private final Path path;
 
         FileLoader(String certificateFolder) {
-            this.path = new File(certificateFolder);
+            this.path = Paths.get(certificateFolder);
         }
 
         @Override
-        public void forEachFile(ForFile forEachFile) throws IOException {
-            if (!this.path.isDirectory()) {
+        public void forEachFile(ForFile forEachFile) {
+            if (!isDirectory(path)) {
                 throw new ConfigurationException("Certificate path '" + this.path + "' is not a directory. " +
                         "It should point to a directory containing certificates.");
             }
-            File[] files = this.path.listFiles();
-            if (files == null) {
-                throw new ConfigurationException("Unable to read certificates from '" + path + "'. Make sure it's the correct path.");
-            }
 
-            for (File file : files) {
-                try (InputStream contents = new FileInputStream(file)) {
-                    forEachFile.call(file.getName(), contents);
-                }
+            try (Stream<Path> files = Files.list(path)) {
+                files.forEach(file -> {
+                    try (InputStream contents = Files.newInputStream(file)) {
+                        forEachFile.call(generateAlias(file), contents);
+                    } catch (Exception e) {
+                        throw new ConfigurationException("Unable to load certificate from file " + file, e);
+                    }
+                });
+            } catch (IOException e) {
+                throw new ConfigurationException("Error reading certificates from " + path, e);
             }
         }
     }
 
     private interface ResourceLoader {
-        void forEachFile(ForFile forEachFile) throws IOException;
+        void forEachFile(ForFile forEachFile);
     }
 
-    private abstract static class ForFile {
-        abstract void call(String fileName, InputStream contents);
+    @FunctionalInterface
+    private interface ForFile {
+        void call(String fileName, InputStream contents) throws IOException, GeneralSecurityException;
+    }
+
+    private static String generateAlias(Path location) {
+        return stream(location.normalize().spliterator(), false)
+                .reduce((e1, e2) -> e1.getFileName().resolve(e2))
+                .map(nameBase -> stream(nameBase.spliterator(), false).map(Path::toString).collect(joining(":")))
+                .orElseGet(() -> UUID.randomUUID().toString());
     }
 
 }
